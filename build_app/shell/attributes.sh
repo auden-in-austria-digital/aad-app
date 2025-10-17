9fbfd91a87275ba67129a5321f46369845c8e2c1
likeanga <angelika.hechtl@gmail.com>
2025-10-17 16:58:57 +0200

    process files in subdirectories (cleanup script) or whatever

--- build_app/shell/attributes.sh
+++ build_app/shell/attributes.sh
@@ -2,4 +2,4 @@
 
 echo "add attributes"
 add-attributes -g "./data/amp/editions/*/*.xml" -b "https://aad.acdh.oeaw.ac.at"
-add-attributes -g "./data/aad/editions/*.xml" -b "https://aad.acdh.oeaw.ac.at"
+add-attributes -g "./data/aad/editions/*/*.xml" -b "https://aad.acdh.oeaw.ac.at"
